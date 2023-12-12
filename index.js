052797dd856e04600729a93a3eda590f2a1e1a38
Lucrezia92 <lucrezia9226@gmail.com>
2023-12-12 07:58:46 +0000

    made changes on JS and html because of mispells

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function searchCity(city){
     
 
 function handleSearchSubmit(event){
-    event.preventDeafault();
+    event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
     let cityElement = document.querySelector("#city");
     cityElement.innerHTML = searchInput.value;
