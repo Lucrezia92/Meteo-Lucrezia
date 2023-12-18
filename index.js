7df0116f0893d1975c1bf9c3eaede073719ad49a
Lucrezia92 <lucrezia9226@gmail.com>
2023-12-18 23:48:03 +0000

    added time and icon

--- index.js
+++ index.js
@@ -7,23 +7,35 @@ function refreshWeather(response) {
     let windspeedElement = document.querySelector("#wind-speed");
     let timeElement = document.querySelector("#time");
     let date = new Date(response.data.time * 1000);
+    let iconElement = document.querySelector("#icon");
     descriptionElement.innerHTML = response.data.condition.description;
     humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
     windspeedElement.innerHTML = `${response.data.wind.speed}km/h`;
     timeElement.innerHTML = formatDate(date);
-  }
+    iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
+}
+  
 
- function formatDate(){
-  let day = days[date.getDay()];
+function formatDate(date) {
   let minutes = date.getMinutes();
   let hours = date.getHours();
-  let days = ["Sunday", "Monday", "Tuesday", "Wednesday","Thursday","Friday","Saturday"];
-  
-  return `${day} ${hours} ${minutes}`;
-   
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
 
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
 
- }
+  return `${day} ${hours}:${minutes}`;
+}
 
   function searchCity(city) {
     let apiKey = "52o0ta3a4163e34b468085936bcf4c78";
