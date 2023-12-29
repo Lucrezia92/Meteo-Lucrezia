a071af3c06b564e3653900ce4519de9afcca20b3
Lucrezia92 <lucrezia9226@gmail.com>
2023-12-29 08:31:18 +0000

    ID forcast added

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 function refreshWeather(response) {
-    let temperatureElement = document.querySelector("#temperature");
-    let temperature = response.data.temperature.current;
-    temperatureElement.innerHTML = Math.round(temperature);
-    let descriptionElement = document.querySelector("#description");
-    let humidityElement = document.querySelector("#humidity");
-    let windspeedElement = document.querySelector("#wind-speed");
-    let timeElement = document.querySelector("#time");
-    let date = new Date(response.data.time * 1000);
-    let iconElement = document.querySelector("#icon");
-    descriptionElement.innerHTML = response.data.condition.description;
-    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
-    windspeedElement.innerHTML = `${response.data.wind.speed}km/h`;
-    timeElement.innerHTML = formatDate(date);
-    iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  temperatureElement.innerHTML = Math.round(temperature);
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windspeedElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windspeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+  timeElement.innerHTML = formatDate(date);
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
 }
-  
+
 
 function formatDate(date) {
   let minutes = date.getMinutes();
@@ -37,38 +37,37 @@ function formatDate(date) {
   return `${day} ${hours}:${minutes}`;
 }
 
-  function searchCity(city) {
-    let apiKey = "52o0ta3a4163e34b468085936bcf4c78";
-    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(refreshWeather);
-  }
-    
-    
-    
-
-function handleSearchSubmit(event){
-    event.preventDefault();
-    let searchInput = document.querySelector("#search-form-input");
-    let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = searchInput.value;
-    searchCity(searchInput.value);
+function searchCity(city) {
+  let apiKey = "52o0ta3a4163e34b468085936bcf4c78";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
 }
 
 
-function displayForecast {
 
-let forecast = document.querySelector("#forecast");
 
-let days = ["Tue","Wed","Thu","Fri","Sat"];
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
+}
 
-let forecastHtml = " ";
 
-days.forEach(function(day) {
+function displayForecast() {
 
-  forecastHtml = 
-  
-  forecastHtml + `<div class="weather-forecast">
-<div class="row">
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  let forecastHtml = '<div class="row">';
+
+  days.forEach(function (day) {
+
+    forecastHtml =
+
+      forecastHtml + `
   <div class="col-2">
     <div class="date">${day}</div>
     <img
@@ -80,13 +79,12 @@ days.forEach(function(day) {
       <div class="weather-temperature"></div>
     </div>
   </div>
-</div>
-</div>
 `;
 
-});
+  });
 
-forecastElement.innerHTML = forecastHtml;
+  forecastHtml = forecastHtml + "</div>"
+  forecastElement.innerHTML = forecastHtml;
 
 }
 
